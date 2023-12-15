e3883f2201e3e5b8d28a6801a504172590c17466
vaibhav-OP <vaibhavyash28@gmail.com>
2023-12-15 16:26:59 +0530

    (Enchanced) output.

--- index.py
+++ index.py
@@ -6,13 +6,16 @@ from colorama import Fore, Style
 mycon = sql.connect(host="localhost", database="journal", user="root", password="123456")
 cursor = mycon.cursor()
 
-def notifyError(text):
+def clearConsole():
     os.system("cls")
+
+def notifyError(text):
+    clearConsole()
     print(Fore.RED + text)
     print(Style.RESET_ALL)
 
 def notifySuccess(text):
-    os.system("cls")
+    clearConsole()
     print(Fore.GREEN + text)
     print(Style.RESET_ALL)
 
@@ -31,28 +34,52 @@ def createJournal():
         cursor.execute("insert into journals(title, description, createdAt) values('{}', '{}', '{}')".format(title, description, currentDate))
         notifySuccess()
 
+def viewTodaysJournal():
+    currentDate = date.today()
+
+    cursor.execute("select * from journals where createdAt = '{}'".format(currentDate))
+    todayJournal = cursor.fetchone()
+
+    if todayJournal == None:
+        notifyError("You have not posted a journal today.")
+    else:
+        clearConsole()
+        print(todayJournal)
+
 commandList = [
     {
         "ID": "1",
         "desc": "Create a journal for your day (you can only create one/day)",
         "call": createJournal,
+    },
+    {
+        "ID": "2",
+        "desc": "Displays today's journal",
+        "call": viewTodaysJournal,
     }
 
 ]
 
 def main():
-    print("Hey, How's your days?")
-
     while True:
         print("Please select a command by typing the prefixed number.")
 
         for command in commandList:
-            print("{id} - {desc}".format(id=command["ID"], desc=command["desc"]))
+            print(Fore.CYAN + "{id} -".format(id=command["ID"]), end=Style.RESET_ALL+" ")
+            print(Fore.GREEN + "{desc}".format(desc=command["desc"]), end=Style.RESET_ALL+"\n")
+        print(Fore.CYAN + "X -", end=Style.RESET_ALL+" ")
+        print(Fore.GREEN + "Exit the application.", end=Style.RESET_ALL+"\n")
+
         inputCommand = input("Enter what you want to do: ")
+        if inputCommand.lower() == "x":
+            break
 
         for command in commandList:
             if inputCommand == command["ID"] :
                 command["call"]()
+                print()
                 break
-
+        else:
+            clearConsole()
+    clearConsole()
 main()
